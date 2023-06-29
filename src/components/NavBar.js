ccc3ac1d21e27420053a830689f3780f7784723a
ASWINBABUKV <aswinbabukv1@gmail.com>
2023-06-29 16:10:49 +0530

    navbar created

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,29 @@
-import { Navbar, Container } from "react-bootstrap";
-export const NavBAr = () => {};
+import "./navbarstyle.css";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className="nav-menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
