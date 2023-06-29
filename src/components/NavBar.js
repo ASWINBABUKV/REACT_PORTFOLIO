548a76f199bc22a61f3f1dea1ab8834d0d97e03f
ASWINBABUKV <aswinbabukv1@gmail.com>
2023-06-29 21:44:28 +0530

    navbar ready

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,27 @@
 import "./navbarstyle.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav-menu">
+      <div
+        className={`hamburger ${isOpen ? "open" : ""}`}
+        onClick={handleClick}
+      >
+        <FaBars size={20} style={{ color: "#fff" }} />
+      </div>
+      <ul className={`nav-menu ${isOpen ? "open" : ""}`}>
         <li>
           <Link to="/">Home</Link>
         </li>
