496e73075ee0af555c8c26c73982eb4f73e9b436
ASWINBABUKV <aswinbabukv1@gmail.com>
2023-06-29 23:51:27 +0530

    made navbar perfect, added home content

---
+++ src/components/Who.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Who.css";
+import { Link } from "react-router-dom";
+const Who = () => {
+  return (
+    <div className="profile">
+      <div className="mask">
+        <img
+          className="intro-img"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/04d53f80577741.5cf8b5c84827c.gif"
+          alt="Introimg"
+        />
+      </div>
+      <div className="content">
+        <h1>ASWIN BABU K V</h1>
+        <p>WEB DEVELOPER</p>
+        <div>
+          <Link to="/project" className="btn">
+            PROJECTS
+          </Link>
+          <Link to="/contact" className="btn-light">
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Who;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Who from "../components/Who";
+
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Who />
     </div>
   );
 };
